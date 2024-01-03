872095ea7045f594c7bafde3f5d035432f981cb0
Leonardo Loureiro <83887814+LeoLoureiro-code@users.noreply.github.com>
2024-01-02 20:54:00 -0800

    Add files via upload

---
+++ Todo.DataAccess.EF/Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Todo.DataAccess.EF.Models;
+
+public partial class ToDo
+{
+    public int TaskId { get; set; }
+
+    public string TaskName { get; set; } = null!;
+}
